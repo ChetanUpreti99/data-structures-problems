8a713c53508172de2a6f6e48350a5f5ee02a60af
ChetanUpreti99 <chetanupreti97@gmail.com>
2021-07-17 19:09:19 +0530

    2-sums-0 problem

---
+++ 2-sums-0.js
@@ -0,0 +1,16 @@
+const inputArr = [-5, -4, -3, -2, 0, 2, 4, 6, 8];
+
+function checkTwoSumZero(inputArr) {
+    for (let i = 0; i < inputArr.length; i++) {
+        for (let j = i + 1; j < inputArr.length; j++) {
+            if (inputArr[i] + inputArr[j] === 0) {
+                return [inputArr[i], inputArr[j]];
+            }
+        }
+    }
+    return;
+}
+
+
+const pairs = checkTwoSumZero([...inputArr]);
+console.log(pairs)
