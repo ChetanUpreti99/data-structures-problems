7b42df413c3462ba98b5d266af6fd10e8e793612
Chetan Upreti <chetanupreti@ip-192-168-29-72.ap-south-1.compute.internal>
2022-11-17 09:07:23 +0530

    feat:findLargestSum

---
+++ largestSum.js
@@ -0,0 +1,22 @@
+let arr = [1, 2, 3, 4, 3, 5, 4, 6, 7, 8];
+
+const findLargestSum = (arr, target) => {
+    if (arr.length < target || target <= 0) {
+        throw new Error(`wrong input`);
+    }
+
+    let max = 0;
+    //number of times loop will run is arr.length-num+1
+    for (let index = 0; index < arr.length - target + 1; index++) {
+        let temp = 0;
+        for (let j = 0; j < target; j++) {
+            temp = temp + arr[index + j];
+        }
+        if (temp > max) {
+            max = temp;
+        }
+    }
+    return max;
+}
+
+console.log(findLargestSum(arr, 4));
