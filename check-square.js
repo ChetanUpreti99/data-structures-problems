e6bf1b3a11b783e29c0b5cec45764c4ae6b46ff3
Chetan Upreti <chetanupreti@ip-192-168-29-72.ap-south-1.compute.internal>
2022-11-19 10:42:18 +0530

    feat:isSquaresCheck

---
+++ check-square.js
@@ -0,0 +1,48 @@
+let firstArr = [1, 2, 3, 4, 5, 5];
+let secondArr = [1, 4, 9, 16, 25];
+
+
+console.log(isSquaresCheck(firstArr, secondArr));
+/* 
+function isSquaresCheck(firstArr, secondArr) {
+    const secondArrMap = new Map(secondArr.map((value, index) => [value, index]));
+    for (const element of firstArr) {
+        if (!secondArrMap.has(element * element)) {
+            return false;
+        } else {
+            continue;
+        }
+    }
+    return true;
+}
+ */
+
+
+
+function isSquaresCheck(firstArr, secondArr) {
+    const firstArrMap = new Map();
+
+    for (const value of firstArr) {
+        firstArrMap.set(value, firstArrMap.get(value) ? firstArrMap.get(value) + 1 : 1)
+    }
+
+    const secondArrMap = new Map();
+
+    for (const value of secondArr) {
+        secondArrMap.set(value, secondArrMap.get(value) ? secondArrMap.get(value) + 1 : 1)
+    }
+
+    // console.log(`firstArrMap`, firstArrMap, `secondArrMap`, secondArrMap);
+
+    for (const [key, value] of firstArrMap) {
+        if (!secondArrMap.has(key * key) || secondArrMap.get(key * key) != value) {
+            return false;
+        } else {
+            continue;
+        }
+    }
+    return true;
+}
+
+
+
