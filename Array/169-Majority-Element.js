74bae73066e5827ee0b7b7e515c7d217244c5e23
ChetanUpreti99 <chetanupreti97@gmail.com>
2024-07-23 14:22:40 +0530

    feat:majorityElement

--- Array/169-Majority-Element.js
+++ Array/169-Majority-Element.js
@@ -13,6 +13,7 @@ Example 2:
 Input: nums = [2,2,1,1,1,2,2]
 Output: 2
  */
+
 function majorityElement(nums) {
     let ele;
     let count = 0;
@@ -34,3 +35,14 @@ let nums = [3, 2, 3];
 console.log(majorityElement(nums));
 nums = [2, 2, 1, 1, 1, 2, 2];
 console.log(majorityElement(nums));
+/**
+Moore’s Voting Algorithm:
+Time Complexity: O(N) + O(N), where N = size of the given array.
+Reason: The first O(N) is to calculate the count and find the expected majority element.
+The second one is to check if the expected element is the majority one or not.
+
+Note: If the question states that the array must contain a majority element, 
+in that case, we do not need the second check. Then the time complexity will boil down to O(N).
+
+Space Complexity: O(1) as we are not using any extra space.
+ */
